c108ec19bec403876a610b68d0c3124d4f48c1a2
Jmr328 <Robersonj267@yahoo.com>
2023-11-06 15:54:54 -0500

    first commit

---
+++ area.py
@@ -0,0 +1,7 @@
+def printArea(l, w):
+    return l * w
+
+# l= int(input("How long is the house? "))
+# w= int(input("How wide is the house? "))
+
+# printArea(l, w)
---
+++ circumference.py
@@ -0,0 +1,6 @@
+def Circum():
+    from math import pi
+    return 2*pi*r
+
+# r = int(input("What is the radius of the circle? "))
+# Circum()
